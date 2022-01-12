e4e9e29c1c1bc04cdd431e2f76a0436914366219
Cyytrus <antoniopaulo122@gmail.com>
2022-01-12 17:46:12 -0300

    Add article test and factory values

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Article, type: :model do
     article = create(:article)
     expect(article.title).to eq("Sample article")
   end
+  
+  it "tests that factory is valid" do
+    article = create(:article)
+    expect(article).to be_valid # article.valid? == true
+  end
 end
