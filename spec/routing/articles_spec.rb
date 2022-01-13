dbe767674892ec8eb5a76fd4680f863290f01e73
Cyytrus <antoniopaulo122@gmail.com>
2022-01-13 08:31:22 -0300

    add tests, routing and articles controller

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe '/articles/routes' do
+  it 'routes to articles#index' do
+    # expect(get '/articles').to route_to(controller: 'articles', acrtion: 'index')
+    expect(get '/articles').to route_to('articles#index')
+    expect(get '/articles?page[number]=3').to(
+      route_to('articles#index', page: { 'number' => '3' })
+    )
+  end
+end
