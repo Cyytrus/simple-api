dc122599aff8766c0fd71189ce689c144e1b6ed5
Cyytrus <antoniopaulo122@gmail.com>
2022-01-13 14:04:54 -0300

    Add: requests tests folder and file, article serialization, jsonapi serializer and update articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  
   resources :articles do
     resources :index, :show
   end
-
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
